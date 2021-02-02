cdd2e84e8b6c5e63ab808ac3417d7ff578e222cb
kaitlyn summerill <kaitlynsummerill@gmail.com>
2021-02-02 07:53:53 -0800

    project plan controller, delete view folder

--- app/controllers/api/v1/project_plans_controller.rb
+++ app/controllers/api/v1/project_plans_controller.rb
@@ -1,4 +1,26 @@
 class Api::V1::ProjectPlansController < ApplicationController
 
+  def index
+      project_plans = ProjectPlan.all
+
+      render json: ProjectPlanSerializer.new(project_plans)
+    end
+
+    def create
+      # raise params.inspect
+      project_plan = ProjectPlan.new(project_plan_params)
+      if project_plan.save
+        render json: ProjectPlanSerializer.new(project_plans), status: :accepted
+      else
+        render json: {errors: project_plan.errors.full_messages}, status: :unprocessible_entity
+      end
+    end
+
+    private
+
+    def project_plan_params
+      # byebug
+      params.require(:project_plan).permit(:name)
+    end
 
 end
