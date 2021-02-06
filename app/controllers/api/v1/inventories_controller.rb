926b613eba273db358062f38ee5869e76f50d694
kaitlyn summerill <kaitlynsummerill@gmail.com>
2021-02-06 09:26:26 -0800

    update inventory controller

--- app/controllers/api/v1/inventories_controller.rb
+++ app/controllers/api/v1/inventories_controller.rb
@@ -1,13 +1,38 @@
 class Api::V1::InventoriesController < ApplicationController
   def index
     inventories = Inventory.all
-
     render json: InventorySerializer.new(inventories)
+  end
+
+  def create
+    inventory = Inventory.new(inventory_params)
+    if inventory.save
+      render json: InventorySerializer.new(inventories), status: :accepted
+    else
+      render json: {errors: inventory.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  def show
+    inventory = Inventory.find(params[:id])
+    render json: inventory
+  end
+
+  def update
+    inventory = Inventory.find(params[:id])
+    inventory.update(name: params["inventory"]["name"], description: params["inventory"]["description"])
+    inventory.save
+    render json: inventory
+  end
+
+  def destroy
+    inventory = Inventory.find(params[:id])
+    inventory.destroy
   end
 
   private
 
   def inventory_params
-    params.require(:inventory).permit(:name, :description, :project_id)
+    params.require(:inventory).permit(:name, :description, :inventory_id)
   end
 end
