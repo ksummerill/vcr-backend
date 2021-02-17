1b73be631b452bbb0e8ed733dd400dd0d9abb577
kaitlyn summerill <kaitlynsummerill@gmail.com>
2021-02-16 19:13:33 -0800

    add before action to inventories controller

--- app/controllers/api/v1/inventories_controller.rb
+++ app/controllers/api/v1/inventories_controller.rb
@@ -1,14 +1,17 @@
 class Api::V1::InventoriesController < ApplicationController
+
+  before_action :find_project
+
   def index
     inventories = Inventory.all
     render json: inventories
   end
 
   def create
-    @inventory = Inventory.new(inventory_params)
+    @inventory = @project.inventories.new(inventory_params)
 
     if @inventory.save
-      render json: @inventory, status: :accepted
+      render json: @project, status: :accepted
     else
       render json: {errors: @inventory.errors.full_messages}, status: :unprocessible_entity
     end
@@ -32,6 +35,10 @@ class Api::V1::InventoriesController < ApplicationController
   end
 
   private
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
 
   def inventory_params
     params.require(:inventory).permit(:name, :description, :inventory_id, :project_id)
